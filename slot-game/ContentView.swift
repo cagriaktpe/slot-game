6a2d98a852558b6dc1673844e98eaa237039694f
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-03-14 22:39:21 +0100

    first commit

--- slot-game/ContentView.swift
+++ slot-game/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    // variables
+    @State var credits = 1000
+    @State var image1 = "apple"
+    @State var image2 = "cherry"
+    @State var image3 = "star"
+    let fruits = ["apple", "cherry", "star"]
+        
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("SwiftUI Slots!")
+                .font(.largeTitle)
+
+            Spacer()
+            
+            Text("Credits: " + String(credits))
+                .font(.title2)
+            
+            Spacer()
+            
+            HStack {
+                Image(image1)
+                    .resizable()
+                    .scaledToFit()
+                Image(image2)
+                    .resizable()
+                    .scaledToFit()
+                Image(image3)
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding()
+            
+            Spacer()
+            
+            Button {
+                changeImages()
+                
+                if image1 == image2 && image1 == image3 {
+                    credits += 15
+                } else {
+                    credits -= 5
+                }
+            } label: {
+                Text("Spin")
+                    .padding(.horizontal)
+            }
+            .buttonStyle(.bordered)
+            .background(.pink)
+            .cornerRadius(25)
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            
+            Spacer()
+
         }
         .padding()
+    }
+    
+    // methods
+    func changeImages() {
+        let random1 = Int.random(in: 0...2)
+        let random2 = Int.random(in: 0...2)
+        let random3 = Int.random(in: 0...2)
+        
+        image1 = fruits[random1]
+        image2 = fruits[random2]
+        image3 = fruits[random3]
     }
 }
 
