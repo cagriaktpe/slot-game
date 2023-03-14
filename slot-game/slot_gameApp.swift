2dcad6725a13038ac08c5f1ad83db8ff3e9b5c12
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-03-14 21:22:02 +0100

    Initial Commit

---
+++ slot-game/slot_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_gameApp.swift
+//  slot-game
+//
+//  Created by Samet Cagri Aktepe on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct slot_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
